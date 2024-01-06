50c6a5fc8fa48efe6884847afd009308982c0d13
sara-coelho <65710155+sara-coelho@users.noreply.github.com>
2024-01-06 11:28:33 +0000

    Create index_2.php

---
+++ index_2.php
@@ -0,0 +1,15 @@
+<head>
+    <link rel="stylesheet" href="assets/styles/style_geral.css">
+</head>
+<body>
+    <?php include("includes/new_menu.php");?>
+    <div class="fundo-roxo">
+        <h1 class="titulo-branco">TITULO BRANCO</h1>
+        <p class="texto-branco">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque finibus sapien vitae ligula accumsan interdum eu eget enim. Quisque interdum pretium aliquet. Fusce sit amet nisi eget odio condimentum ultrices. Nam nunc nisl, mattis ac massa a, elementum facilisis arcu. Integer viverra rutrum lorem, sit amet faucibus mi lobortis ac. Vestibulum sed eleifend risus, sed sollicitudin augue. In sit amet metus ipsum. Donec bibendum, elit quis mattis luctus, lectus felis convallis nulla, at imperdiet orci mi ac dui. Nam ullamcorper sagittis aliquet. Cras eu feugiat nulla, et convallis velit. Curabitur sit amet enim faucibus, tempor justo non, malesuada nisi. Suspendisse.</p>
+    </div>
+    <div class="fundo-branco">
+        <h1 class="titulo-roxo">TITULO ROXO</h1>
+        <p class="texto-roxo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque finibus sapien vitae ligula accumsan interdum eu eget enim. Quisque interdum pretium aliquet. Fusce sit amet nisi eget odio condimentum ultrices. Nam nunc nisl, mattis ac massa a, elementum facilisis arcu. Integer viverra rutrum lorem, sit amet faucibus mi lobortis ac. Vestibulum sed eleifend risus, sed sollicitudin augue. In sit amet metus ipsum. Donec bibendum, elit quis mattis luctus, lectus felis convallis nulla, at imperdiet orci mi ac dui. Nam ullamcorper sagittis aliquet. Cras eu feugiat nulla, et convallis velit. Curabitur sit amet enim faucibus, tempor justo non, malesuada nisi. Suspendisse.</p>
+    </div>
+    <?php include("includes/new_footer.php");?>
+</body>
