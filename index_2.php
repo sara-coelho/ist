a8c4cbe73e79b0e9973010a5505fe72374a2dc8b
sara-coelho <65710155+sara-coelho@users.noreply.github.com>
2024-01-28 00:39:16 +0000

    Update index_2.php

--- index_2.php
+++ index_2.php
@@ -3,12 +3,8 @@
 </head>
 <body>
     <?php include("includes/new_menu.php");?>
-    <div class="fundo-roxo">
-        <h1 class="titulo-branco">TITULO BRANCO</h1>
-        <p class="texto-branco">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque finibus sapien vitae ligula accumsan interdum eu eget enim. Quisque interdum pretium aliquet. Fusce sit amet nisi eget odio condimentum ultrices. Nam nunc nisl, mattis ac massa a, elementum facilisis arcu. Integer viverra rutrum lorem, sit amet faucibus mi lobortis ac. Vestibulum sed eleifend risus, sed sollicitudin augue. In sit amet metus ipsum. Donec bibendum, elit quis mattis luctus, lectus felis convallis nulla, at imperdiet orci mi ac dui. Nam ullamcorper sagittis aliquet. Cras eu feugiat nulla, et convallis velit. Curabitur sit amet enim faucibus, tempor justo non, malesuada nisi. Suspendisse.</p>
-    </div>
     <div class="fundo-branco">
-        <h1 class="titulo-roxo">TITULO ROXO</h1>
+        <h1 class="titulo-roxo">O que é a JOB2BE?</h1>
         <p class="texto-roxo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque finibus sapien vitae ligula accumsan interdum eu eget enim. Quisque interdum pretium aliquet. Fusce sit amet nisi eget odio condimentum ultrices. Nam nunc nisl, mattis ac massa a, elementum facilisis arcu. Integer viverra rutrum lorem, sit amet faucibus mi lobortis ac. Vestibulum sed eleifend risus, sed sollicitudin augue. In sit amet metus ipsum. Donec bibendum, elit quis mattis luctus, lectus felis convallis nulla, at imperdiet orci mi ac dui. Nam ullamcorper sagittis aliquet. Cras eu feugiat nulla, et convallis velit. Curabitur sit amet enim faucibus, tempor justo non, malesuada nisi. Suspendisse.</p>
     </div>
     <?php include("includes/new_footer.php");?>
