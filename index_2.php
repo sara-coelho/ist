89668d5afe11006db2616b8a7ef1d14b41691ce3
sara-coelho <65710155+sara-coelho@users.noreply.github.com>
2024-02-02 23:23:23 +0000

    Update index_2.php

--- index_2.php
+++ index_2.php
@@ -9,8 +9,10 @@
             <tr>
                 <td style="text-align: left;">
                     <h1 style="font-size: 2em;" class="titulo-roxo">O que é a JOB2BE?</h1>
+                    <div class="item">
+                        <button class="menu-button"><a href="sobre.php">Saber +</a></button>
+                    </div>
                 </td>
-                <button class="menu-button"><a href="sobre.php">Button Text</a></button>
                 <td style="text-align: right;">
                     <p class="texto-roxo">
                         A JOB2BE - Semana da Biomédica 2024 é um evento organizado pelo Núcleo de Estudantes de Engenharia Biomédica do Instituto Superior Técnico da Universidade de Lisboa (NEBM-IST) que visa estreitar a relação entre a comunidade académica, científica e o mundo empresarial, assim como divulgar o trabalho desenvolvido atualmente na área da Engenharia Biomédica.
