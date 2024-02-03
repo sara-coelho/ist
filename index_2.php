04ac9967dc6925c1b01f50dcff9572168b134847
sara-coelho <65710155+sara-coelho@users.noreply.github.com>
2024-02-03 18:42:49 +0000

    Update index_2.php

--- index_2.php
+++ index_2.php
@@ -5,25 +5,17 @@
 <body>
     <?php include("includes/new_menu.php");?>
     <div class="fundo-branco">
-        <table style="width: 100%;">
-            <tr>
-                <td style="text-align: center; vertical-align: middle;">
-                    <div class="imagem">
-                        <img src="assets/img/o_que_e_roxo.png">
-                    </div>
-                    <div class="item">
-                        <a href="sobre.php">
-                            <button class="menu-button" style="background-color: #3F165B; color: #FFFFFF; border-radius: 15px;">saber +</button>
-                        </a>
-                    </div>
-                </td>
-                <td style="text-align: left;">
-                    <p class="texto-roxo">
-                        A JOB2BE - Semana da Biomédica 2024 é um evento organizado pelo Núcleo de Estudantes de Engenharia Biomédica do Instituto Superior Técnico da Universidade de Lisboa (NEBM-IST) que visa estreitar a relação entre a comunidade académica, científica e o mundo empresarial, assim como divulgar o trabalho desenvolvido atualmente na área da Engenharia Biomédica.
-                    </p>
-                </td>
-            </tr>
-        </table>
+        <div class="imagem">
+            <img src="assets/img/o_que_e_roxo.png">
+        </div>
+        <div class="item">
+            <a href="sobre.php">
+                <button class="menu-button" style="background-color: #3F165B; color: #FFFFFF; border-radius: 15px;">saber +</button>
+            </a>
+        </div>
+        <div class="texto-roxo">
+            A JOB2BE - Semana da Biomédica 2024 é um evento organizado pelo Núcleo de Estudantes de Engenharia Biomédica do Instituto Superior Técnico da Universidade de Lisboa (NEBM-IST) que visa estreitar a relação entre a comunidade académica, científica e o mundo empresarial, assim como divulgar o trabalho desenvolvido atualmente na área da Engenharia Biomédica.
+        </div>
     </div>
     <?php include("includes/new_footer.php");?>
 </body>
